1f039e5be33952a3351f1334c8f52f6e39cf856c
takenokoya <takuya1002@gmail.com>
2020-04-29 23:50:53 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='roboter',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='free',
+    author='sakaguchitakuya',
+    author_email='takuya1002@gmail.com',
+    description=''
+)
